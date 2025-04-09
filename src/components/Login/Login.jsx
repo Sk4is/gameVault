72b18a60fe1d6f97fd2004daf81f5faa1f3349c8
Adrián Pérez Agredano <adrianperezagredano@gmail.com>
2025-04-09 12:25:49 +0200

    dev merged

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "./login.css";
+import "./Login.css";
 import Swal from "sweetalert2";
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
