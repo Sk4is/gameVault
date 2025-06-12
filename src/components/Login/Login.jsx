2c18cc6f0505ce06995628150452852043b47e18
Adrián Pérez Agredano <adrianperezagredano@gmail.com>
2025-06-12 14:05:19 +0200

    enviroment variables changed

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -46,7 +46,7 @@ const Login = () => {
     setLoading(true);
 
     try {
-      const response = await axios.post("/api/login", { email, password });
+      const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/login`, { email, password });
       const { token } = response.data;
 
       localStorage.setItem("token", token);
