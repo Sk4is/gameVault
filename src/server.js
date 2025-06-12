6304ad8a7a391ff2e5e1354777f6c89442ff469c
Adrián Pérez Agredano <adrianperezagredano@gmail.com>
2025-06-12 13:01:30 +0200

    connection Railway

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://tu-frontend-en-vercel.vercel.app"],
     credentials: true,
   })
 );
